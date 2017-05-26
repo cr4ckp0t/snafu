8eadb4828938b0f9b93477050364cd669bc1c9e9
Adam Koch <ank14a@ghs.org>
2017-05-26 08:11:53 -0400

    testing visual studio code branch management

--- js/popup.js
+++ js/popup.js
@@ -16,6 +16,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
 
+// branch test
+
 var wildcards = {
     "{ASSIGN_GROUP}": "{ASSIGN_GROUP} - Current assignment group.",
     "{OPENED}": "{OPENED} - Date and time the incident or task was opened.",
